84db50ae39408b041dabd8c33b3e31bd8a9a9bc7
Christine Koulopoulos <christinekoul1@gmail.com>
2022-01-27 19:03:54 -0500

    letting user choose length

--- wordle.py
+++ wordle.py
@@ -1,37 +1,38 @@
 from random_word import RandomWords
 
-def grab_soln():
+def grab_soln(length):
     r = RandomWords()
 
     # Return a single random word
-    soln = r.get_random_word(hasDictionaryDef="true",minLength=5, maxLength=5, minCorpusCount=10000)
+    soln = r.get_random_word(hasDictionaryDef="true",minLength=length, maxLength=length, minCorpusCount=10000)
     while (soln[0].isupper() or (not soln.isalpha())):
-        soln = r.get_random_word(hasDictionaryDef="true",minLength=5, maxLength=5, minCorpusCount=10000)
+        soln = r.get_random_word(hasDictionaryDef="true",minLength=length, maxLength=length, minCorpusCount=10000)
 
     return soln
 
 def print_results(guess, results):
     # TO DO: make this prettier
-    for i in range(0,5):
-        print("{0} ".format(guess[i]),end='')
+    for letter in guess:
+        print("{0} ".format(letter),end='')
     print()
-    for i in range(0,5):
-        print("{0} ".format(results[i]),end='')
+    for result in results:
+        print("{0} ".format(result),end='')
     print()
 
 def main():
     # TO DO: let user choose length of word
+    length = int(input("enter length of word: "))
     # grab a random 5 letter word
-    soln = grab_soln()
+    soln = grab_soln(length)
 
-    for i in range(0,6):
+    for i in range(0,5):
         # ask user for guess
         guess = input("enter guess #{0}: ".format(i+1))
         # TO DO: validate guess (length, actual word)
 
         # iterate through guess, tracking results (yes=2, no=0, close=1)
-        results = [0,0,0,0,0]
-        for i in range(0,5):
+        results = [0] * length
+        for i in range(0,length):
             if (guess[i] == soln[i]):
                 results[i] = 2
             elif (guess[i] in soln):
@@ -44,7 +45,7 @@ def main():
             print("YOU WON - CONGRATULATIONS!")
             return
 
-    print("YOU LOST :(")
+    print("YOU LOST - THE SOLUTION WAS {0}".format(soln))
 
 if __name__ == "__main__":
     main()
